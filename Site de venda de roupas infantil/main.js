07a7bed908b8e8f95d240ea87f996e3064344d40
Beatriz Vieira <gomes.pessoa@escola.pr.gov.br>
2025-05-15 11:45:05 -0300

    Add files via upload

---
+++ Site de venda de roupas infantil/main.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentloaded', function(){
+    const hamburger = document.getElementById ('hamburger');
+    const navMenu = document.getElementById ('navMenu');
+
+
+    hamburger.addEventListener('click', function(){
+      hamburger.classList.toggle('active');
+      navMenu.classList.toggle('active');
+     });
+
+     // Fechar o menu quando um item for clicado
+     const navItem = document.querySelectorAll('nav-item');
+     navItem.forEach (item => {
+        item.addEventListener('click', function(){
+            hamburger.classList.remove('active');
+            navMenu.classList.remove('active');
+            });
+        });
+     });
