de5fc5cb4e8f7ea0d28e2fe6ab03046167a6279e
Beatriz Vieira <gomes.pessoa@escola.pr.gov.br>
2025-09-19 12:18:46 -0300

    Add files via upload
    Trabalho de progressão de html e css para javascript. Lista de Compra

---
+++ A/script.js
@@ -0,0 +1,41 @@
+
+
+//cria o titulo
+const titulo = document.createElement('h1');
+titulo.textContent = 'Lista de Compras';
+document.body.appendChild(titulo);
+
+const inputItem = document.createElement('input');
+inputItem.type = 'text';
+inputItem.placeholder = ' Digite um item';
+document.body.appendChild(inputItem);
+
+const btnAdd = document.createElement('button');
+btnAdd.textContent = 'Adicionar';
+document.body.appendChild(btnAdd);
+
+const lista = document.createElement('ul');
+document.body.appendChild(lista);
+
+
+function adicionarItem() {
+    const valor =  inputItem.value.trim();
+    if (valor !== '') {
+        const li = document.createElement('li');
+        li.textContent= valor;
+        lista.appendChild(li);
+        inputItem.value = '';
+        inputItem.focus(); // volta o foco para o campo
+    }
+};
+
+btnAdd.addEventListener('click', adicionarItem);
+
+inputItem.addEventListener('keydown', (event) =>{
+    if (event.key === 'Enter'){
+        adicionarItem();
+    }
+});
+
+
+
