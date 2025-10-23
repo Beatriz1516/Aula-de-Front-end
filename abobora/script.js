a013264ff332e274ffc111180b70b506e20130f8
Beatriz Vieira <gomes.pessoa@escola.pr.gov.br>
2025-10-23 08:07:17 -0300

    NOVO EXERCÍCIO CRIADO DO ZERO

---
+++ abobora/script.js
@@ -0,0 +1,8 @@
+const titulo = document.createElement = ('h1');
+titulo.id = 'titulo';
+titulo.textContent = 'titulo';
+document.body.appendChild(titulo);
+
+titulo.style.backgroundColor = 'red';
+
+const texto = document.createElement = ('p');
