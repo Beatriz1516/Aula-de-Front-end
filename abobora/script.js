b05672bf15b8616e9a6d59ffa0ce7a09df85b198
Beatriz Vieira <gomes.pessoa@escola.pr.gov.br>
2025-10-24 08:09:26 -0300

    Fix element creation syntax and add paragraph

--- abobora/script.js
+++ abobora/script.js
@@ -1,8 +1,10 @@
-const titulo = document.createElement = ('h1');
+const titulo = document.createElement=('h1');
 titulo.id = 'titulo';
-titulo.textContent = 'titulo';
 document.body.appendChild(titulo);
 
 titulo.style.backgroundColor = 'red';
+titulo.style.color = 'white';
 
-const texto = document.createElement = ('p');
+const texto = document.createElement=('p');
+texto.id = 'Esse parágrafo é para ser mostrado de forma simples e corriqueira.';
+document.body.appendChild(texto);
