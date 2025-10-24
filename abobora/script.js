1e7cf3c14f7cad4785bdc86e7f400a6ec19f41fb
Beatriz Vieira <gomes.pessoa@escola.pr.gov.br>
2025-10-24 11:44:28 -0300

    Imagem
    foi refeito e agora aparece os textos e a imagem, com borda arredondada e alaranjada.

--- abobora/script.js
+++ abobora/script.js
@@ -1,10 +1,19 @@
-const titulo = document.createElement=('h1');
-titulo.id = 'titulo';
+const titulo = document.createElement('h1');
+titulo.textContent = 'APLICAÇÃO DE JAVASCRIPT';
+titulo.style.textAlign = 'center';
+titulo.style.color = 'orange';
 document.body.appendChild(titulo);
 
-titulo.style.backgroundColor = 'red';
-titulo.style.color = 'white';
-
-const texto = document.createElement=('p');
-texto.id = 'Esse parágrafo é para ser mostrado de forma simples e corriqueira.';
+const texto = document.createElement('p');
+texto.textContent = 'Nessa aplicação estamos testando na prática a codificação de javascript.';
+texto.style.textAlign = 'center';
 document.body.appendChild(texto);
+
+const imagem = document.createElement('img');
+imagem.src = '/imagens/imagem-abobora.jpg';
+imagem.style.width = '400px';
+imagem.style.margin = '50px 50px 50px 720px';
+imagem.style.border = 'solid 3px darkOrange';
+imagem.style.borderRadius = '300px';
+imagem.style.boxShadow = '0 10px 15px rgb(0, 0, 0, 0.3)'
+document.body.appendChild(imagem);
